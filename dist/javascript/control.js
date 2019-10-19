6355211df65246ac7cc6ae8c436b1cf08bc3dd90
Arthur Wolf <wolf.arthur@gmail.com>
2019-10-19 22:12:46 +0200

    rebuilding after picking project back up

--- dist/javascript/control.js
+++ dist/javascript/control.js
@@ -1,16 +1,18 @@
-class Control{
-  constructor(parent){
+class Control {
+  constructor(parent) {
     this.machine = parent;
   }
 
-  jog(p){ // P is parameters
+  jog(p) { // P is parameters
     // Jog the axes
-    if( p.direction == '+' ){ p.direction = ''; }
+    if (p.direction == '+') {
+      p.direction = '';
+    }
     var command = p.axis + p.direction + p.distance;
     this.machine.communication.command(["M120", "G91", `G0 ${command}`, 'M121'], true, false).then(() => {});
   }
 
-  home(axis){
+  home(axis) {
     // TODO: Make this dependent upon CNC mode or not
 
   }
