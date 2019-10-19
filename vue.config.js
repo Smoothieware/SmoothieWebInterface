d6bfe7ec447ce6f019c13beb0388f4a3b56c17f6
Arthur Wolf <wolf.arthur@gmail.com>
2019-10-19 22:29:43 +0200

    attempting to fix the wrong path on github pages compared to local serving

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  //publicPath: process.env.NODE_ENV === 'production' ? '/SmoothieWebInterface/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/SmoothieWebInterface/dist/' : '/',
   pages: {
     index: {
       // entry for the page
